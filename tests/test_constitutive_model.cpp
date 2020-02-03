06ab26f91c19714195b099156e579f1fcd4f0a0b
David Levin <dilevin@csail.mit.edu>
2020-02-02 20:53:35 -0500

    make test a bit more difficult to pass

--- tests/test_constitutive_model.cpp
+++ tests/test_constitutive_model.cpp
@@ -28,15 +28,20 @@ int main(int argc, char **argv) {
     //test some random dXinvs by building random B's and multiplying by q's
     //simulation state
     Eigen::MatrixXd Vt = V.transpose();
+    Vt.row(0) *= 2;
+    Vt.row(1) *= 4;
+    Vt.row(2) *= 3;
+
     Eigen::VectorXd q = Eigen::Map<Eigen::VectorXd>(Vt.data(), V.size(),1);
 
 
     Eigen::Vector12d q_ele;
     Eigen::Vector9d test_I;
-    test_I <<1,0,0,0,1,0,0,0,1;
+    test_I <<2,0,0,0,4,0,0,0,3;
 
     double error;
 
+    //test deformation gradient calculation 
     for(unsigned int ii=0; ii<E.rows(); ++ii) {
         q_ele << q.segment<3>(3*E(ii,0)), q.segment<3>(3*E(ii,1)), q.segment<3>(3*E(ii,2)), q.segment<3>(3*E(ii,3));
         error = (sim::flatten_multiply_right<Eigen::Matrix<double, 3,4>>(sim::unflatten<4,3>(dXinv.row(ii)))*q_ele - test_I).norm();
@@ -48,4 +53,9 @@ int main(int argc, char **argv) {
     }
 
     std::cout<<"PASSED\n";
+
+    //to do assemble a stiffness matrix for fun
+
+    //assemble the force vector (which should be zero in the underformed state)
+
 }
