4da29a84f469417fbdb1ca03c6abb7f8ed9c4186
David Levin <dilevin@csail.mit.edu>
2020-03-07 18:22:05 -0500

    update comments and example for fixed point matrix

--- matlab/fixed_point_constraint_matrix.m
+++ matlab/fixed_point_constraint_matrix.m
@@ -4,12 +4,12 @@
 %
 % Inputs:
 %    V  #T by 3 list of vertices
-%    fixeD_indices #FIXED by 1 list of indices into V, indicating fixed vertices 
+%    fixeD_indices #FIXED by 1 - list of indices into V SORTED IN ASCENDING ORDER, indicating fixed vertices 
 % Outputs:
 %    P  (size(V,1)*size(V,2) - size(V,2)*#FIXED) by size(V,1)*size(V,2) sparse matrix 
 %
 % Example:
-%   % mesh (V,T)
-%   vol = volume(V,T); %using gptoolbox volume command
-%   rho =  0.1*ones(size(T,1),1);
-%   M = linear_tetmesh_mass_matrix(V,T, rho, vol);
+%  % mesh (V,T)
+%  % list of 3d points C
+%  [min_d, min_I] = min(bsxfun(@minus, V(:,1), C(:,1)').^2 + bsxfun(@minus, V(:,2), C(:,2)').^2,[],1); %find vertices nearest to points in list
+%  P = fixed_point_constraint_matrix(V, sort(min_I))
