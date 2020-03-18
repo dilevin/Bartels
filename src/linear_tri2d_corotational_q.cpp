69f3f2548b23c6649023c969400755978a970261
David Levin <dilevin@csail.mit.edu>
2020-03-17 22:08:22 -0400

    fix some bugs in energy

--- src/linear_tri2d_corotational_q.cpp
+++ src/linear_tri2d_corotational_q.cpp
@@ -9,6 +9,6 @@ auto sim::linear_tri2d_corotational_q(const Eigen::MatrixBase<DerivedV> &q, cons
     Eigen::Vector6x<Scalar> qe; qe << q.segment(2*element(0),2), q.segment(2*element(1),2), q.segment(2*element(2),2); //qe
     Eigen::Matrix3x<typename DefoType::Scalar> F = Eigen::Matrix3x<typename DerivedV::Scalar>::Zero();  Eigen::Vector9x<typename DerivedV::Scalar> one = Eigen::Vector9x<typename DerivedV::Scalar>::Zero(); one(8) = 1.0; Eigen::Matrix<typename DerivedV::Scalar, 9,6> P; P<<1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0; F.block(0,0,3,2) = dXinv; Eigen::Matrix<typename DefoType::Scalar, 9,6> B = sim::flatten_multiply_right<Eigen::Matrix<typename DefoType::Scalar, 3,3> >(F)*P; //compute B
 
-    return psi_stretch_F(unflatten<3,3>((B*qe + one).eval()), [](auto &a, auto &b) { return sim::psi_corotational_S(a,b); }, params);
+    return volume*psi_stretch_F(unflatten<3,3>((B*qe + one).eval()), [](auto &a, auto &b) { return sim::psi_corotational_S(a,b); }, params);
 
 }
