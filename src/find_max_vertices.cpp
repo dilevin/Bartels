e8f20712135c6b398aaed9c959d62b8d5a04bbd3
David Levin <dilevin@csail.mit.edu>
2020-12-27 23:42:36 -0500

    choose dimension for max vertex search

--- src/find_max_vertices.cpp
+++ src/find_max_vertices.cpp
@@ -3,17 +3,17 @@
 #endif
 
 template<typename DerivedV, typename Scalar>
-void sim::find_max_vertices(std::vector<int> &indices, const Eigen::DenseBase<DerivedV> &V, Scalar tol) {
+void sim::find_max_vertices(std::vector<int> &indices, const Eigen::DenseBase<DerivedV> &V, unsigned int dim, Scalar tol) {
 
-    Scalar max_vertex = V(0,1); 
+    Scalar max_vertex = V(0,dim); 
     
     for(unsigned int vi=0; vi<V.rows(); ++vi) {
-        max_vertex = (V(vi,1) > max_vertex ? V(vi,1) : max_vertex);
+        max_vertex = (V(vi,dim) > max_vertex ? V(vi,dim) : max_vertex);
     }
 
     for(unsigned int vi=0; vi<V.rows(); ++vi) {
 
-        if(std::abs(V(vi,1)-max_vertex) <= tol) {
+        if(std::abs(V(vi,dim)-max_vertex) <= tol) {
             indices.push_back(vi);
         }
     }
--- src/find_min_vertices.cpp
+++ src/find_min_vertices.cpp
@@ -3,17 +3,17 @@
 #endif
 
 template<typename DerivedV, typename Scalar>
-void sim::find_min_vertices(std::vector<int> &indices, const Eigen::DenseBase<DerivedV> &V, Scalar tol) {
+void sim::find_min_vertices(std::vector<int> &indices, const Eigen::DenseBase<DerivedV> &V, unsigned int dim, Scalar tol) {
 
-    Scalar min_vertex = V(0,1); 
+    Scalar min_vertex = V(0,dim); 
     
     for(unsigned int vi=0; vi<V.rows(); ++vi) {
-        min_vertex = (V(vi,1) < min_vertex ? V(vi,1) : min_vertex);
+        min_vertex = (V(vi,dim) < min_vertex ? V(vi,dim) : min_vertex);
     }
 
     for(unsigned int vi=0; vi<V.rows(); ++vi) {
 
-        if(std::abs(V(vi,1)-min_vertex) <= tol) {
+        if(std::abs(V(vi,dim)-min_vertex) <= tol) {
             indices.push_back(vi);
         }
     }
