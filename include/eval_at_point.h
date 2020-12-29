506a1dec5fd490abb3be0dff5433832ae52d67c6
David Levin <dilevin@csail.mit.edu>
2020-12-28 20:32:42 -0500

    eval_at_point multiple parameters

--- include/eval_at_point.h
+++ include/eval_at_point.h
@@ -3,10 +3,17 @@
 
 #include <Eigen/Dense>
 #include <EigenTypes.h>
-
+#include <relay_params.h>
 #include <flatten.h>
 
 namespace sim {
+
+    template<typename DerivedRet, typename DerivedV,
+             int Rows, int Cols, int Options, int MaxRows, int MaxCols,
+            typename Func2, typename DerivedX, typename ...Params>
+    void eval_at_point(Eigen::MatrixXx<DerivedRet> &results, const Eigen::MatrixBase<DerivedV> &V, Eigen::Ref<const Eigen::MatrixXi> E, 
+                       Func2 func, Eigen::Matrix<DerivedRet, Rows, Cols, Options, MaxRows, MaxCols> tmp, 
+                       const Eigen::MatrixBase<DerivedX> &X, const Params & ... params);
 
     template<typename DerivedRet, typename DerivedV,
              int Rows, int Cols, int Options, int MaxRows, int MaxCols,
--- src/eval_at_point.cpp
+++ src/eval_at_point.cpp
@@ -6,6 +6,32 @@
 
 template<typename DerivedRet, typename DerivedV, 
          int Rows, int Cols, int Options, int MaxRows, int MaxCols,
+         typename Func2, typename DerivedX,
+         typename ... Params >
+void sim::eval_at_point(Eigen::MatrixXx<DerivedRet> &results, const  Eigen::MatrixBase<DerivedV> &V, Eigen::Ref<const Eigen::MatrixXi> E, 
+                        Func2 func, Eigen::Matrix<DerivedRet, Rows, Cols, Options, MaxRows, MaxCols> tmp, 
+                        const Eigen::MatrixBase<DerivedX> &X, const Params & ... params) {
+
+    assert(E.rows() == X.rows());
+
+    results.resize(E.rows(), flatten(tmp).rows());
+
+    auto eval_func = [&func, &V](auto &tmp, auto &e, auto &x, auto & ... params) 
+                           { 
+                             func(tmp, V, e, x, params ...);
+                           };
+
+    for(unsigned int ie=0; ie< E.rows(); ++ie) {
+        //func(tmp, V, E.row(ie), X.row(ie));
+        //use relay params so that I can send arbitrary parameters to each method
+        relay_params(ie, eval_func, tmp, E, X, params ...);
+        results.row(ie) = flatten(tmp).transpose();
+    }
+
+}
+
+template<typename DerivedRet, typename DerivedV, 
+         int Rows, int Cols, int Options, int MaxRows, int MaxCols,
          typename Func2, typename DerivedX>
 void sim::eval_at_point(Eigen::MatrixXx<DerivedRet> &results, const  Eigen::MatrixBase<DerivedV> &V, Eigen::Ref<const Eigen::MatrixXi> E, 
                         Func2 func, Eigen::Matrix<DerivedRet, Rows, Cols, Options, MaxRows, MaxCols> tmp, 
@@ -15,9 +41,16 @@ void sim::eval_at_point(Eigen::MatrixXx<DerivedRet> &results, const  Eigen::Matr
 
     results.resize(E.rows(), flatten(tmp).rows());
 
+    auto eval_func = [&func, &V](auto &tmp, auto &e, auto &x) 
+                           { 
+                             func(tmp, V, e, x);
+                           };
+
     for(unsigned int ie=0; ie< E.rows(); ++ie) {
-        func(tmp, V, E.row(ie), X.row(ie));
+        //func(tmp, V, E.row(ie), X.row(ie));
+        //use relay params so that I can send arbitrary parameters to each method
+        relay_params(ie, eval_func, tmp, E, X);
         results.row(ie) = flatten(tmp).transpose();
     }
-
+                        
 }
