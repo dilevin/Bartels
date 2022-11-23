dda387b78aaa75bd812de970af8374daa36b5c4d
David Levin <dlevin@nvidia.com>
2022-11-23 10:45:57 -0500

    first untested python binding

---
+++ python/linear_tetmesh_mass_matrix.cpp
@@ -0,0 +1,21 @@
+#include <npe.h>
+
+//bartels
+#include <linear_tetmesh_mass_matrix.h>
+
+npe_function(linear_tetmesh_mass_matrix)
+npe_arg(V, dense_float, dense_double)
+npe_arg(E, dense_int)
+npe_arg(densities, npe_matches(V))
+npe_arg(volumes, npe_matches(V))
+npe_begin_code()
+
+    Eigen::SparseMatrix<npe_Scalar_V> M;
+    Eigen::Matrix<npe_Scalar_V, Eigen::Dynamic, 1> densities_vec = densities.col(0);
+    Eigen::Matrix<npe_Scalar_V, Eigen::Dynamic, 1> volumes_vec = volumes.col(0);
+
+    sim::linear_tetmesh_mass_matrix(M, V, E, densities_vec, volumes_vec);
+
+    return std::make_tuple(npe::move(M));
+
+npe_end_code()
--- src/linear_tet_mass_matrix.cpp
+++ src/linear_tet_mass_matrix.cpp
@@ -9,9 +9,9 @@ template<typename MassMatrixType, typename  Scalar>
 void sim::linear_tet_mass_matrix(Eigen::DenseBase<MassMatrixType> &M, Eigen::Ref<const Eigen::RowVectorXi> element, Scalar density, Scalar volume) {
      
     //setup the mass matrix
-    double mass = density*volume;
-    double c0 = (1.0/10.0)*mass;
-    double c1 = (1.0/20.0)*mass;
+    Scalar mass = density*volume;
+    Scalar c0 = (1.0/10.0)*mass;
+    Scalar c1 = (1.0/20.0)*mass;
 
     //m_massMatrix
     //Point Indices
@@ -21,24 +21,24 @@ void sim::linear_tet_mass_matrix(Eigen::DenseBase<MassMatrixType> &M, Eigen::Ref
     unsigned int p3 = 9;
 
     //Assemble these bad boys //really big 4x4 block matrix
-    M.block(p0,p0, 3,3) = c0*Eigen::Matrix<double,3,3>::Identity();
-    M.block(p0,p1, 3,3) = c1*Eigen::Matrix<double,3,3>::Identity();
-    M.block(p0,p2, 3,3) = c1*Eigen::Matrix<double,3,3>::Identity();
-    M.block(p0,p3, 3,3) = c1*Eigen::Matrix<double,3,3>::Identity();
+    M.block(p0,p0, 3,3) = c0*Eigen::Matrix<Scalar,3,3>::Identity();
+    M.block(p0,p1, 3,3) = c1*Eigen::Matrix<Scalar,3,3>::Identity();
+    M.block(p0,p2, 3,3) = c1*Eigen::Matrix<Scalar,3,3>::Identity();
+    M.block(p0,p3, 3,3) = c1*Eigen::Matrix<Scalar,3,3>::Identity();
 
-    M.block(p1,p0, 3,3) = c1*Eigen::Matrix<double,3,3>::Identity();
-    M.block(p1,p1, 3,3) = c0*Eigen::Matrix<double,3,3>::Identity();
-    M.block(p1,p2, 3,3) = c1*Eigen::Matrix<double,3,3>::Identity();
-    M.block(p1,p3, 3,3) = c1*Eigen::Matrix<double,3,3>::Identity();
+    M.block(p1,p0, 3,3) = c1*Eigen::Matrix<Scalar,3,3>::Identity();
+    M.block(p1,p1, 3,3) = c0*Eigen::Matrix<Scalar,3,3>::Identity();
+    M.block(p1,p2, 3,3) = c1*Eigen::Matrix<Scalar,3,3>::Identity();
+    M.block(p1,p3, 3,3) = c1*Eigen::Matrix<Scalar,3,3>::Identity();
 
-    M.block(p2,p0, 3,3) = c1*Eigen::Matrix<double,3,3>::Identity();
-    M.block(p2,p1, 3,3) = c1*Eigen::Matrix<double,3,3>::Identity();
-    M.block(p2,p2, 3,3) = c0*Eigen::Matrix<double,3,3>::Identity();
-    M.block(p2,p3, 3,3) = c1*Eigen::Matrix<double,3,3>::Identity();
+    M.block(p2,p0, 3,3) = c1*Eigen::Matrix<Scalar,3,3>::Identity();
+    M.block(p2,p1, 3,3) = c1*Eigen::Matrix<Scalar,3,3>::Identity();
+    M.block(p2,p2, 3,3) = c0*Eigen::Matrix<Scalar,3,3>::Identity();
+    M.block(p2,p3, 3,3) = c1*Eigen::Matrix<Scalar,3,3>::Identity();
 
-    M.block(p3,p0, 3,3) = c1*Eigen::Matrix<double,3,3>::Identity();
-    M.block(p3,p1, 3,3) = c1*Eigen::Matrix<double,3,3>::Identity();
-    M.block(p3,p2, 3,3) = c1*Eigen::Matrix<double,3,3>::Identity();
-    M.block(p3,p3, 3,3) = c0*Eigen::Matrix<double,3,3>::Identity();
+    M.block(p3,p0, 3,3) = c1*Eigen::Matrix<Scalar,3,3>::Identity();
+    M.block(p3,p1, 3,3) = c1*Eigen::Matrix<Scalar,3,3>::Identity();
+    M.block(p3,p2, 3,3) = c1*Eigen::Matrix<Scalar,3,3>::Identity();
+    M.block(p3,p3, 3,3) = c0*Eigen::Matrix<Scalar,3,3>::Identity();
 
  }
